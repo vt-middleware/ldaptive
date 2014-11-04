2b86a211cadca4a495530fa4c1080382966a255a
Daniel Fisher <dfisher@vt.edu>
2014-11-04 12:47:03 -0500

    Add ldapi module.

---
+++ ldapi/src/main/java/org/ldaptive/ldapi/AFUnixSocketFactory.java
@@ -0,0 +1,167 @@
+/*
+  $Id: AFUnixSocketFactory.java 3008 2014-07-02 14:36:23Z dfisher $
+
+  Copyright (C) 2003-2014 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision: 3008 $
+  Updated: $Date: 2014-07-02 10:36:23 -0400 (Wed, 02 Jul 2014) $
+*/
+package org.ldaptive.ldapi;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.URLDecoder;
+import javax.net.SocketFactory;
+import org.newsclub.net.unix.AFUNIXSocket;
+import org.newsclub.net.unix.AFUNIXSocketAddress;
+
+/**
+ * AFUnixSocketFactory is an extension of SocketFactory which utilizes {@link
+ * AFUNIXSocket} to connect to a host, it is intended for local file-based
+ * native socket connections only.
+ *
+ * @author  Middleware Services
+ * @version  $Revision: 3008 $ $Date: 2014-07-02 10:36:23 -0400 (Wed, 02 Jul 2014) $
+ */
+public class AFUnixSocketFactory extends SocketFactory
+{
+
+  /** File system location of the domain socket. */
+  private final String socketFile;
+
+
+  /**
+   * Creates a new AFUnixSocketFactory configured with the system property
+   * 'org.ldaptive.ldapi.socketFile'.
+   */
+  public AFUnixSocketFactory()
+  {
+    this(System.getProperty("org.ldaptive.ldapi.socketFile"));
+  }
+
+
+  /**
+   * Creates a new AFUnixSocketFactory.
+   *
+   * @param  socket  file system location of the domain socket
+   */
+  public AFUnixSocketFactory(final String socket)
+  {
+    socketFile = socket;
+  }
+
+
+  /**
+   * This returns a new instance of AFUnixSocketFactory. See {@link
+   * #AFUnixSocketFactory()}.
+   *
+   * @return  socket factory
+   */
+  public static SocketFactory getDefault()
+  {
+    return new AFUnixSocketFactory();
+  }
+
+
+  /**
+   * Creates a new instance of AFUNIXSocket using the host as the file system
+   * location of the domain socket. The host parameter is ignored if {@link
+   * #socketFile} is set. The port parameter is always ignored.
+   *
+   * @param  host  ignored if {@link #socketFile} is set
+   * @param  port  Unsupported, will be ignored
+   *
+   * @return  unix socket
+   *
+   * @throws  IOException  if the file system location cannot be URL decoded
+   */
+  @Override
+  public Socket createSocket(final String host, final int port)
+    throws IOException
+  {
+    File file;
+    if (socketFile != null) {
+      file = new File(URLDecoder.decode(socketFile, "UTF-8"));
+    } else {
+      file = new File(URLDecoder.decode(host, "UTF-8"));
+    }
+
+    final AFUNIXSocketAddress localAddress = new AFUNIXSocketAddress(file);
+    final AFUNIXSocket sock = AFUNIXSocket.newInstance();
+    sock.connect(localAddress);
+    return sock;
+  }
+
+
+  /**
+   * This method is not supported.
+   *
+   * @param  host  Unsupported.
+   * @param  port  Unsupported.
+   * @param  localHost  Unsupported.
+   * @param  localPort  Unsupported.
+   *
+   * @return  Socket Unsupported.
+   *
+   * @throws  IOException  Unsupported.
+   */
+  @Override
+  public Socket createSocket(
+    final String host,
+    final int port,
+    final InetAddress localHost,
+    final int localPort)
+    throws IOException
+  {
+    throw new UnsupportedOperationException("This method is not supported.");
+  }
+
+
+  /**
+   * This method is not supported.
+   *
+   * @param  host  Unsupported.
+   * @param  port  Unsupported.
+   *
+   * @return  Socket Unsupported.
+   *
+   * @throws  IOException  Unsupported.
+   */
+  @Override
+  public Socket createSocket(final InetAddress host, final int port)
+    throws IOException
+  {
+    throw new UnsupportedOperationException("This method is not supported.");
+  }
+
+
+  /**
+   * This method is not supported.
+   *
+   * @param  address  Unsupported.
+   * @param  port  Unsupported.
+   * @param  localAddress  Unsupported.
+   * @param  localPort  Unsupported.
+   *
+   * @return  Socket Unsupported.
+   *
+   * @throws  IOException  Unsupported.
+   */
+  @Override
+  public Socket createSocket(
+    final InetAddress address,
+    final int port,
+    final InetAddress localAddress,
+    final int localPort)
+    throws IOException
+  {
+    throw new UnsupportedOperationException("This method is not supported.");
+  }
+}
---
+++ ldapi/src/test/java/org/ldaptive/ldapi/PerformanceTest.java
@@ -0,0 +1,154 @@
+/*
+  $Id: PerformanceTest.java 3011 2014-07-02 15:15:42Z dfisher $
+
+  Copyright (C) 2003-2014 Virginia Tech.
+  All rights reserved.
+
+  SEE LICENSE FOR MORE INFORMATION
+
+  Author:  Middleware Services
+  Email:   middleware@vt.edu
+  Version: $Revision: 3011 $
+  Updated: $Date: 2014-07-02 11:15:42 -0400 (Wed, 02 Jul 2014) $
+*/
+package org.ldaptive.ldapi;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.ldaptive.Connection;
+import org.ldaptive.ConnectionConfig;
+import org.ldaptive.ConnectionFactory;
+import org.ldaptive.DefaultConnectionFactory;
+import org.ldaptive.LdapEntry;
+import org.ldaptive.SearchOperation;
+import org.ldaptive.SearchRequest;
+import org.ldaptive.SearchResult;
+import org.ldaptive.provider.jndi.JndiProvider;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+/**
+ * Performance test for an ldap search operation over native sockets.
+ *
+ * @author  Middleware Services
+ * @version  $Revision: 3011 $ $Date: 2014-07-02 11:15:42 -0400 (Wed, 02 Jul 2014) $
+ */
+public class PerformanceTest
+{
+
+  /** LDAP host. */
+  private String ldapHost;
+
+  /** LDAP base dn. */
+  private String ldapBaseDn;
+
+  /** LDAP search filter. */
+  private String ldapSearchFilter;
+
+
+  /**
+   * Initialize this test.
+   *
+   * @param  socketFile  file system location of the unix domain socket
+   * @param  host  for comparison
+   * @param  baseDn  to search on
+   * @param  searchFilter  to execute
+   */
+  @BeforeClass(groups = {"ldapi"})
+  @Parameters(
+    {
+      "ldapSocketFile",
+      "ldapTestHost",
+      "ldapBaseDn",
+      "ldapSearchFilter"
+    }
+  )
+  public void initialize(
+    final String socketFile,
+    final String host,
+    final String baseDn,
+    final String searchFilter)
+  {
+    System.setProperty("org.ldaptive.ldapi.socketFile", socketFile);
+    ldapHost = host;
+    ldapBaseDn = baseDn;
+    ldapSearchFilter = searchFilter;
+  }
+
+
+  /**
+   * Test connection factories.
+   *
+   * @return  test data
+   *
+   * @throws  Exception  on test failure
+   */
+  @DataProvider(name = "factories")
+  public Object[][] connectionFactories()
+    throws Exception
+  {
+    final Map<String, Object> props = new HashMap<String, Object>();
+    props.put(
+      JndiProvider.SOCKET_FACTORY,
+      "org.ldaptive.ldapi.AFUnixSocketFactory");
+
+    final DefaultConnectionFactory nativeFactory = new DefaultConnectionFactory(
+      new ConnectionConfig("ldap://domainsocket"));
+    nativeFactory.getProvider().getProviderConfig().setProperties(props);
+
+    final DefaultConnectionFactory localFactory = new DefaultConnectionFactory(
+      new ConnectionConfig(ldapHost));
+
+    final SearchRequest request = new SearchRequest(
+      ldapBaseDn,
+      ldapSearchFilter);
+    return
+      new Object[][] {
+        new Object[] {nativeFactory, request, },
+        new Object[] {localFactory, request, },
+      };
+  }
+
+
+  /**
+   * Searches with the supplied connection factory.
+   *
+   * @param  connFactory  to get connections from
+   * @param  request  to execute
+   *
+   * @throws  Exception  On test errors.
+   */
+  @Test(
+    groups = {"ldapi"},
+    dataProvider = "factories"
+  )
+  public void search(
+    final ConnectionFactory connFactory,
+    final SearchRequest request)
+    throws Exception
+  {
+    final long beforeTS = System.currentTimeMillis();
+    final Connection conn = connFactory.getConnection();
+    try {
+      conn.open();
+      for (int i = 0; i < 10000; i++) {
+        final SearchOperation search = new SearchOperation(conn);
+        final SearchResult result = search.execute(request).getResult();
+        for (final LdapEntry entry : result.getEntries()) {
+          entry.toString();
+        }
+      }
+    } finally {
+      conn.close();
+    }
+
+    final long afterTS = System.currentTimeMillis();
+    System.out.println(
+      String.format(
+        "Total Execution Time %s ms for %s",
+        afterTS - beforeTS,
+        connFactory));
+  }
+}
