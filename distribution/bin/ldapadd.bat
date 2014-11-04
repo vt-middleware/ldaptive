2984e9b7053c04fda1cdba3ab02d9f916ba3b156
Daniel Fisher <dfisher@vt.edu>
2014-11-04 12:39:07 -0500

    Add module for creating distros.
    Move licenses and notice into distribution module.

---
+++ distribution/bin/ldapadd.bat
@@ -0,0 +1,24 @@
+@echo off
+if "%OS%" == "Windows_NT" setlocal
+
+if not defined JAVA_HOME goto no_java_home
+if not defined LDAPTIVE_HOME goto no_ldaptive_home
+
+set JAVA=%JAVA_HOME%\bin\java
+
+set LDAP_JAR=%LDAPTIVE_HOME%\jars\ldaptive-${project.version}.jar
+set LIBDIR=%LDAPTIVE_HOME%\lib
+
+set CLASSPATH=%LIBDIR%\commons-cli-1.2.jar;%LIBDIR%\slf4j-api-1.7.7.jar;%LDAP_JAR%
+
+call "%JAVA%" -cp "%CLASSPATH%" org.ldaptive.cli.AddOperationCli %*
+goto end
+
+:no_ldaptive_home
+echo ERROR: LDAPTIVE_HOME environment variable must be set to Ldaptive install path.
+goto end
+
+:no_java_home
+echo ERROR: JAVA_HOME environment variable must be set to JRE/JDK install path.
+
+:end
---
+++ distribution/bin/ldapjaas.bat
@@ -0,0 +1,26 @@
+@echo off
+if "%OS%" == "Windows_NT" setlocal
+
+if not defined JAVA_HOME goto no_java_home
+if not defined LDAPTIVE_HOME goto no_ldaptive_home
+
+set JAVA=%JAVA_HOME%\bin\java
+
+set LDAP_JAR=%LDAPTIVE_HOME%\jars\ldaptive-${project.version}.jar
+set LIBDIR=%LDAPTIVE_HOME%\lib
+
+set JAAS_OPTS=-Djava.security.auth.login.config=%LDAPTIVE_HOME%\bin\ldap_jaas.config
+
+set CLASSPATH=%LIBDIR%\commons-cli-1.2.jar;%LIBDIR%\slf4j-api-1.7.7.jar;%LDAP_JAR%
+
+call "%JAVA%" "%JAAS_OPTS%" -cp "%CLASSPATH%" org.ldaptive.jaas.LdapLoginModule %*
+goto end
+
+:no_ldaptive_home
+echo ERROR: LDAPTIVE_HOME environment variable must be set to Ldaptive install path.
+goto end
+
+:no_java_home
+echo ERROR: JAVA_HOME environment variable must be set to JRE/JDK install path.
+
+:end
