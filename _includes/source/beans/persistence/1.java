ac9b715bd1fe325788ce87eb1ea20402ff042716
Daniel Fisher <dfisher@vt.edu>
2015-11-03 17:31:56 -0500

    Include this class in integration test source.

--- _includes/source/beans/persistence/1.java
+++ _includes/source/beans/persistence/1.java
@@ -11,28 +11,3 @@ manager.add(addObject);
 MyObject mergeObject = manager.find(new MyObject("uid=dfisher,ou=people,dc=ldaptive,dc=org"));
 mergeObject.setPhoneNumber("555-555-1234");
 manager.merge(mergeObject);
-
-// simple object containing LDAP attribute data
-@Entry(
-  dn = "dn",
-  attributes = {
-    @Attribute(name = "name"),
-    @Attribute(name = "email"),
-    @Attribute(name = "phoneNumber")})
-public class MyObject {
-  private String dn;
-  private String name;
-  private String email;
-  private String phoneNumber;
-
-  public MyObject() {}
-  public MyObject(String s) { setDn(s); }
-  public String getDn() { return dn; }
-  public void setDn(String d) { dn = s; }
-  public String getName() { return name; }
-  public void setName(String s) { name = s; }
-  public String getEmail() { return email; }
-  public void setEmail(String s) { email = s; }
-  public String getPhoneNumber() { return phoneNumber; }
-  public void setPhoneNumber(String s) { phoneNumber = s; }
-}
