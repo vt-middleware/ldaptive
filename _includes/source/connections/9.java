c31b86b5b626196337f9f445a718328522fc1879
Daniel Fisher <dfisher@vt.edu>
2016-02-25 17:18:35 -0500

    Update for connection strategy change.

--- _includes/source/connections/9.java
+++ _includes/source/connections/9.java
@@ -1,3 +1,4 @@
-DefaultConnectionFactory connFactory = new DefaultConnectionFactory("ldap://directory-1.ldaptive.org ldap://directory-2.ldaptive.org");
-connFactory.getProvider().getProviderConfig().setConnectionStrategy(ConnectionStrategy.ROUND_ROBIN);
+ConnectionConfig connConfig = new ConnectionConfig("ldap://directory-1.ldaptive.org ldap://directory-2.ldaptive.org");
+connConfig.setConnectionStrategy(new RoundRobinConnectionStrategy());
+DefaultConnectionFactory connFactory = new DefaultConnectionFactory(connConfig);
 Connection conn = connFactory.getConnection();
