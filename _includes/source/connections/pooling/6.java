e83ae3db2bb92440163a8f47d3722b9bba3f4ace
Daniel Fisher <dfisher@vt.edu>
2016-04-11 16:16:27 -0400

    Update docs for Duration syntax.

--- _includes/source/connections/pooling/6.java
+++ _includes/source/connections/pooling/6.java
@@ -1,3 +1,3 @@
 BlockingConnectionPool pool = new BlockingConnectionPool(new DefaultConnectionFactory("ldap://directory.ldaptive.org"));
-pool.setPruneStrategy(new IdlePruneStrategy(900, 1800));
+pool.setPruneStrategy(new IdlePruneStrategy(Duration.ofMinutes(15), Duration.ofMinutes(30)));
 pool.initialize();
