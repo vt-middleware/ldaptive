046b4c3035518b95b4720d031beb65aca82731f0
Daniel Fisher <dfisher@vt.edu>
2016-11-01 17:44:03 -0400

    Revert for API compat.

--- integration/src/test/java/org/ldaptive/handler/NoOpEntryHandler.java
+++ integration/src/test/java/org/ldaptive/handler/NoOpEntryHandler.java
@@ -1,6 +1,8 @@
 /* See LICENSE for licensing and NOTICE for copyright. */
 package org.ldaptive.handler;
 
+import org.ldaptive.LdapUtils;
+
 /**
  * Entry handler that does nothing.
  *
@@ -8,4 +10,14 @@ package org.ldaptive.handler;
  */
 public class NoOpEntryHandler extends AbstractSearchEntryHandler
 {
+
+  /** hash code seed. */
+  private static final int HASH_CODE_SEED = 887;
+
+
+  @Override
+  public int hashCode()
+  {
+    return LdapUtils.computeHashCode(HASH_CODE_SEED, (Object) null);
+  }
 }
