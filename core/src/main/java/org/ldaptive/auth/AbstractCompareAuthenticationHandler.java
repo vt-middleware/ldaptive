c1c06d4afcd2d77ab4288bbde539dfed8b7f9b29
Daniel Fisher <dfisher@vt.edu>
2017-07-27 13:41:37 -0400

    Support both name and algorithm for compare scheme.

--- core/src/main/java/org/ldaptive/auth/AbstractCompareAuthenticationHandler.java
+++ core/src/main/java/org/ldaptive/auth/AbstractCompareAuthenticationHandler.java
@@ -6,6 +6,7 @@ import java.security.NoSuchAlgorithmException;
 import org.ldaptive.CompareOperation;
 import org.ldaptive.CompareRequest;
 import org.ldaptive.Connection;
+import org.ldaptive.Credential;
 import org.ldaptive.LdapAttribute;
 import org.ldaptive.LdapException;
 import org.ldaptive.LdapUtils;
@@ -20,13 +21,13 @@ public abstract class AbstractCompareAuthenticationHandler extends AbstractAuthe
 {
 
   /** Default password scheme. Value is {@value}. */
-  protected static final String DEFAULT_SCHEME = "SHA";
+  protected static final String DEFAULT_SCHEME = "SHA:SHA";
 
   /** Default password attribute. Value is {@value}. */
   protected static final String DEFAULT_ATTRIBUTE = "userPassword";
 
   /** Password scheme. */
-  private String passwordScheme = DEFAULT_SCHEME;
+  private Scheme passwordScheme = new Scheme(DEFAULT_SCHEME);
 
   /** Password attribute. */
   private String passwordAttribute = DEFAULT_ATTRIBUTE;
@@ -39,18 +40,18 @@ public abstract class AbstractCompareAuthenticationHandler extends AbstractAuthe
    */
   public String getPasswordScheme()
   {
-    return passwordScheme;
+    return passwordScheme.toString();
   }
 
 
   /**
-   * Sets the password scheme. Must equal a known message digest algorithm.
+   * Sets the password scheme.
    *
    * @param  s  password scheme
    */
   public void setPasswordScheme(final String s)
   {
-    passwordScheme = s;
+    passwordScheme = new Scheme(s);
   }
 
 
@@ -82,18 +83,10 @@ public abstract class AbstractCompareAuthenticationHandler extends AbstractAuthe
     final AuthenticationCriteria criteria)
     throws LdapException
   {
-    byte[] hash;
-    try {
-      final MessageDigest md = MessageDigest.getInstance(passwordScheme);
-      md.update(criteria.getCredential().getBytes());
-      hash = md.digest();
-    } catch (NoSuchAlgorithmException e) {
-      throw new LdapException(e);
-    }
-
+    final byte[] hash = digestCredential(criteria.getCredential(), passwordScheme.getAlgorithm());
     final LdapAttribute la = new LdapAttribute(
       passwordAttribute,
-      String.format("{%s}%s", passwordScheme, LdapUtils.base64Encode(hash)).getBytes());
+      String.format("{%s}%s", passwordScheme.getLabel(), LdapUtils.base64Encode(hash)).getBytes());
     final CompareOperation compare = new CompareOperation(c);
     final CompareRequest request = new CompareRequest(criteria.getDn(), la);
     request.setControls(getAuthenticationControls());
@@ -111,6 +104,29 @@ public abstract class AbstractCompareAuthenticationHandler extends AbstractAuthe
 
 
   /**
+   * Digests the supplied credential using the supplied algorithm.
+   *
+   * @param  credential  to digest
+   * @param  algorithm  type of digest to use
+   *
+   * @return  digested credential
+   *
+   * @throws  LdapException  if the supplied algorithm cannot be found
+   */
+  protected byte[] digestCredential(final Credential credential, final String algorithm)
+    throws LdapException
+  {
+    try {
+      final MessageDigest md = MessageDigest.getInstance(algorithm);
+      md.update(credential.getBytes());
+      return md.digest();
+    } catch (NoSuchAlgorithmException e) {
+      throw new LdapException(e);
+    }
+  }
+
+
+  /**
    * Returns a connection that the compare operation should be performed on.
    *
    * @return  connection
@@ -120,4 +136,73 @@ public abstract class AbstractCompareAuthenticationHandler extends AbstractAuthe
   @Override
   protected abstract Connection getConnection()
     throws LdapException;
+
+
+  /**
+   * Represents a password scheme used for attribute comparison.
+   */
+  public static class Scheme
+  {
+
+    /** Label of the scheme. */
+    private final String label;
+
+    /** Algorithm used by this scheme. */
+    private final String algorithm;
+
+
+    /**
+     * Creates a new scheme.
+     *
+     * @param  labelAndAlgorithm  colon delimited label:algorithm
+     */
+    public Scheme(final String labelAndAlgorithm)
+    {
+      final String[] s = labelAndAlgorithm.split(":", 2);
+      label = s[0];
+      algorithm = s.length == 2 ? s[1] : s[0];
+    }
+
+
+    /**
+     * Creates a new scheme.
+     *
+     * @param  l  label
+     * @param  a  algorithm
+     */
+    public Scheme(final String l, final String a)
+    {
+      label = l;
+      algorithm = a;
+    }
+
+
+    /**
+     * Returns the scheme label.
+     *
+     * @return  label
+     */
+    public String getLabel()
+    {
+      return label;
+    }
+
+
+    /**
+     * Returns the scheme algorithm.
+     *
+     * @return  algorithm
+     */
+    public String getAlgorithm()
+    {
+      return algorithm;
+    }
+
+
+    @Override
+    public String toString()
+    {
+      return String.format("%s:%s", label, algorithm);
+    }
+  }
 }
