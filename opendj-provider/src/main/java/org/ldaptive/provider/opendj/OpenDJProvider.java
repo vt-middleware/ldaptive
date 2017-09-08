455f70730682460b0205ca74fc96291a0aba75fb
Daniel Fisher <dfisher@vt.edu>
2017-09-08 18:07:59 -0400

    Update providers to pass new hostname tests.

--- opendj-provider/src/main/java/org/ldaptive/provider/opendj/OpenDJProvider.java
+++ opendj-provider/src/main/java/org/ldaptive/provider/opendj/OpenDJProvider.java
@@ -4,14 +4,17 @@ package org.ldaptive.provider.opendj;
 import java.security.GeneralSecurityException;
 import java.util.concurrent.TimeUnit;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
 import org.forgerock.opendj.ldap.LDAPOptions;
 import org.ldaptive.ConnectionConfig;
 import org.ldaptive.LdapURL;
 import org.ldaptive.provider.Provider;
 import org.ldaptive.provider.ProviderConnectionFactory;
+import org.ldaptive.ssl.CertificateHostnameVerifier;
 import org.ldaptive.ssl.CredentialConfig;
 import org.ldaptive.ssl.DefaultHostnameVerifier;
 import org.ldaptive.ssl.DefaultSSLContextInitializer;
+import org.ldaptive.ssl.HostnameVerifierConfig;
 import org.ldaptive.ssl.HostnameVerifyingTrustManager;
 import org.ldaptive.ssl.SSLContextInitializer;
 
@@ -48,31 +51,46 @@ public class OpenDJProvider implements Provider<OpenDJProviderConfig>
    */
   protected SSLContext getHostnameVerifierSSLContext(final ConnectionConfig cc)
   {
-    SSLContext sslContext;
+    final LdapURL ldapUrl = new LdapURL(cc.getLdapUrl());
     SSLContextInitializer contextInit;
-    if (cc.getSslConfig() != null && cc.getSslConfig().getCredentialConfig() != null) {
-      try {
-        final CredentialConfig credConfig = cc.getSslConfig().getCredentialConfig();
-        contextInit = credConfig.createSSLContextInitializer();
-      } catch (GeneralSecurityException e) {
-        throw new IllegalArgumentException(e);
+    if (cc.getSslConfig() != null && !cc.getSslConfig().isEmpty()) {
+      final CredentialConfig credConfig = cc.getSslConfig().getCredentialConfig();
+      final TrustManager[] managers = cc.getSslConfig().getTrustManagers();
+      final CertificateHostnameVerifier verifier = cc.getSslConfig().getHostnameVerifier();
+      if (credConfig != null) {
+        try {
+          contextInit = credConfig.createSSLContextInitializer();
+        } catch (GeneralSecurityException e) {
+          throw new IllegalArgumentException(e);
+        }
+      } else {
+        if (managers != null) {
+          contextInit = new DefaultSSLContextInitializer(false);
+        } else {
+          contextInit = new DefaultSSLContextInitializer(true);
+        }
+      }
+
+      if (managers != null) {
+        contextInit.setTrustManagers(managers);
+      }
+      if (verifier != null) {
+        contextInit.setHostnameVerifierConfig(new HostnameVerifierConfig(verifier, ldapUrl.getHostnames()));
+      } else {
+        contextInit.setHostnameVerifierConfig(
+          new HostnameVerifierConfig(new DefaultHostnameVerifier(), ldapUrl.getHostnames()));
       }
     } else {
-      contextInit = new DefaultSSLContextInitializer();
-    }
-    if (cc.getSslConfig() != null && cc.getSslConfig().getTrustManagers() != null) {
-      contextInit.setTrustManagers(cc.getSslConfig().getTrustManagers());
-    } else {
-      final LdapURL ldapUrl = new LdapURL(cc.getLdapUrl());
+      contextInit = new DefaultSSLContextInitializer(true);
       contextInit.setTrustManagers(
         new HostnameVerifyingTrustManager(new DefaultHostnameVerifier(), ldapUrl.getHostnames()));
     }
+
     try {
-      sslContext = contextInit.initSSLContext("TLS");
+      return contextInit.initSSLContext("TLS");
     } catch (GeneralSecurityException e) {
       throw new IllegalArgumentException(e);
     }
-    return sslContext;
   }
 
 
