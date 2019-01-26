5316386ce3d6cb0cd61505d6af53d5d83ffb88cf
Daniel Fisher <dfisher@vt.edu>
2019-01-25 22:30:25 -0500

    Add netty I/O implementation.
    Includes a netty buffer implementation and a netty connection implementation.

---
+++ ldaptive-netty/src/main/java/org/ldaptive/io/EncodedRequest.java
@@ -0,0 +1,65 @@
+/* See LICENSE for licensing and NOTICE for copyright. */
+package org.ldaptive.io;
+
+import org.ldaptive.LdapUtils;
+import org.ldaptive.protocol.Request;
+
+/**
+ * Wrapper object that stores an encoded request with it's message ID.
+ *
+ * @author  Middleware Services
+ */
+public class EncodedRequest
+{
+
+  /** Protocol message ID. */
+  private final int messageID;
+
+  /** Encoded request. */
+  private final byte[] encoded;
+
+
+  /**
+   * Creates a new encoded request.
+   *
+   * @param  id  message ID
+   * @param  request  to encode
+   */
+  public EncodedRequest(final int id, final Request request)
+  {
+    messageID = id;
+    encoded = request.encode(messageID);
+  }
+
+
+  /**
+   * Returns the message ID.
+   *
+   * @return  message ID
+   */
+  public int getMessageID()
+  {
+    return messageID;
+  }
+
+
+  /**
+   * Returns the encoded request.
+   *
+   * @return  encoded request.
+   */
+  public byte[] getEncoded()
+  {
+    return encoded;
+  }
+
+
+  @Override
+  public String toString()
+  {
+    return new StringBuilder(
+      getClass().getName()).append("@").append(hashCode()).append("::")
+      .append("messageID=").append(messageID).append(", ")
+      .append("encoded=").append(LdapUtils.hexEncode(encoded)).toString();
+  }
+}
---
+++ ldaptive-netty/src/main/java/org/ldaptive/io/MessageFrameDecoder.java
@@ -0,0 +1,71 @@
+/* See LICENSE for licensing and NOTICE for copyright. */
+package org.ldaptive.io;
+
+import java.util.List;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+import org.ldaptive.asn1.DERBuffer;
+import org.ldaptive.asn1.DERParser;
+import org.ldaptive.asn1.UniversalDERTag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Reads the input byte buffer until an entire message is available.
+ *
+ * @author  Middleware Services
+ */
+public class MessageFrameDecoder extends ByteToMessageDecoder
+{
+
+  /** Logger for this class. */
+  protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+
+  @Override
+  protected void decode(final ChannelHandlerContext ctx, final ByteBuf in, final List<Object> out)
+  {
+    if (in.readableBytes() > 2) {
+      final int readerIdx = in.readerIndex();
+      final int writerIdx = in.writerIndex();
+      final DERBuffer buffer = new NettyDERBuffer(in.readSlice(in.readableBytes()));
+      final int len = readMessageLength(buffer);
+      if (len > 0) {
+        in.readerIndex(readerIdx);
+        out.add(in.readRetainedSlice(len));
+      } else {
+        in.setIndex(readerIdx, writerIdx);
+      }
+    }
+  }
+
+
+  /**
+   * Inspects the supplied buffer for a {@link UniversalDERTag#SEQ} tag and confirms the buffer contains enough bytes
+   * for the length specified for the tag.
+   *
+   * @param  buffer  to read
+   *
+   * @return  DER message length
+   *
+   * @throws  IllegalArgumentException  if the buffer doesn't contain a SEQ tag
+   */
+  private int readMessageLength(final DERBuffer buffer)
+  {
+    final DERParser messageParser = new DERParser();
+    final int tag = messageParser.readTag(buffer).getTagNo();
+    if (UniversalDERTag.SEQ.getTagNo() != tag) {
+      throw new IllegalArgumentException("Invalid message tag: " + tag);
+    }
+    try {
+      final int len = messageParser.readLength(buffer);
+      if (buffer.position() + len <= buffer.capacity()) {
+        return buffer.position() + len;
+      }
+    } catch (Exception e) {
+      logger.trace("Error reading message length", e);
+    }
+    return -1;
+  }
+}
