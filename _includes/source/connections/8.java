2f732b9e62d793954b652bf1c9f605e33d4bbdb4
Daniel Fisher <dfisher@vt.edu>
2019-08-11 23:04:58 -0400

    Fix compilation errors.

--- _includes/source/connections/8.java
+++ _includes/source/connections/8.java
@@ -1,5 +1,5 @@
 ConnectionConfig.builder()
-  .url("ldap://directory-1.ldaptive.org" "ldap://directory-2.ldaptive.org" "ldap://directory-3.ldaptive.org")
+  .url("ldap://directory-1.ldaptive.org ldap://directory-2.ldaptive.org ldap://directory-3.ldaptive.org")
   .useStartTLS(true)
   .connectTimeout(Duration.ofSeconds(5))
   .responseTimeout(Duration.ofSeconds(5))
