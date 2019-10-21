96c2ef30d323fda1998a843cd879b7c8c97b7507
Daniel Fisher <dfisher@vt.edu>
2019-10-20 23:52:35 -0400

    Add support for
    executing message handling on a separate event loop group.
    setting auto_read to false.

--- core/src/main/java/org/ldaptive/provider/netty/NettyProvider.java
+++ core/src/main/java/org/ldaptive/provider/netty/NettyProvider.java
@@ -1,11 +1,15 @@
 /* See LICENSE for licensing and NOTICE for copyright. */
 package org.ldaptive.provider.netty;
 
+import java.util.Map;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import org.ldaptive.Connection;
 import org.ldaptive.ConnectionConfig;
 import org.ldaptive.provider.Provider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Creates netty connections.
@@ -15,36 +19,76 @@ import org.ldaptive.provider.Provider;
 public class NettyProvider implements Provider
 {
 
-  /** Event loop group. */
-  private final EventLoopGroup workerGroup;
+  /** Logger for this class. */
+  protected final Logger logger = LoggerFactory.getLogger(getClass());
 
   /** Channel type. */
-  private final Class<? extends Channel> channelClass;
+  private final Class<? extends Channel> channelType;
+
+  /** Event loop group for I/O, must support the channel type. */
+  private final EventLoopGroup ioWorkerGroup;
+
+  /** Event loop group for message handling. */
+  private final EventLoopGroup messageWorkerGroup;
+
+  /** Override channel options. */
+  private final Map<ChannelOption, Object> channelOptions;
 
 
   /**
    * Creates a new netty provider.
    *
-   * @param  clazz  type of channel
-   * @param  group  event loop group
+   * @param  type  of channel
+   * @param  ioGroup  event loop group to handle I/O
    */
-  public NettyProvider(final Class<? extends Channel> clazz, final EventLoopGroup group)
+  public NettyProvider(final Class<? extends Channel> type, final EventLoopGroup ioGroup)
   {
-    channelClass = clazz;
-    workerGroup = group;
+    this(type, ioGroup, null, null);
+  }
+
+
+  /**
+   * Creates a new netty provider.
+   *
+   * @param  type  of channel
+   * @param  ioGroup  event loop group to handle I/O
+   * @param  messageGroup  event loop group to handle inbound messages, can be null
+   * @param  options  netty channel options
+   */
+  public NettyProvider(
+    final Class<? extends Channel> type,
+    final EventLoopGroup ioGroup,
+    final EventLoopGroup messageGroup,
+    final Map<ChannelOption, Object> options)
+  {
+    channelType = type;
+    ioWorkerGroup = ioGroup;
+    messageWorkerGroup = messageGroup;
+    channelOptions = options;
   }
 
 
   @Override
   public Connection create(final ConnectionConfig cc)
   {
-    return new NettyConnection(channelClass, workerGroup, cc);
+    return new NettyConnection(cc, channelType, ioWorkerGroup, messageWorkerGroup, channelOptions);
   }
 
 
   @Override
   public void close()
   {
-    workerGroup.shutdownGracefully();
+    try {
+      ioWorkerGroup.shutdownGracefully();
+    } catch (Exception e) {
+      logger.warn("Error shutting down the I/O worker group", e);
+    }
+    if (messageWorkerGroup != null) {
+      try {
+        messageWorkerGroup.shutdownGracefully();
+      } catch (Exception e) {
+        logger.warn("Error shutting down the message worker group", e);
+      }
+    }
   }
 }
