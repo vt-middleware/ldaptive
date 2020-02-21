4de96a75a437d60d9b79984785ab1c1456d9e648
Daniel Fisher <dfisher@vt.edu>
2020-02-21 09:49:51 -0500

    Provide override to use NIO.
    Add static method which provides custom shutdown timeouts.

--- core/src/main/java/org/ldaptive/transport/netty/NettyUtils.java
+++ core/src/main/java/org/ldaptive/transport/netty/NettyUtils.java
@@ -1,6 +1,7 @@
 /* See LICENSE for licensing and NOTICE for copyright. */
 package org.ldaptive.transport.netty;
 
+import java.util.concurrent.TimeUnit;
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.Epoll;
@@ -24,6 +25,16 @@ import org.slf4j.LoggerFactory;
 public final class NettyUtils
 {
 
+  /** Time in milliseconds for graceful shutdown quiet period. */
+  private static final long DEFAULT_SHUTDOWN_QUIET_PERIOD = 500;
+
+  /** Time in milliseconds for graceful shutdown max wait. */
+  private static final long DEFAULT_SHUTDOWN_MAX_TIMEOUT = 1000;
+
+  /** Whether to use NIO even if other transports are available. */
+  private static final boolean USE_NIO = Boolean.valueOf(
+    System.getProperty("org.ldaptive.transport.netty.useNio", "false"));
+
   /** Logger for this class. */
   private static final Logger LOGGER = LoggerFactory.getLogger(NettyUtils.class);
 
@@ -36,6 +47,7 @@ public final class NettyUtils
   static {
     LOGGER.debug("Detecting Epoll transport: {}", Epoll.isAvailable());
     LOGGER.debug("Detecting KQueue transport: {}", KQueue.isAvailable());
+    LOGGER.debug("Overriding to use Nio transport: {}", USE_NIO);
   }
 
 
@@ -47,9 +59,9 @@ public final class NettyUtils
    */
   public static Class<? extends Channel> getDefaultSocketChannelType()
   {
-    if (Epoll.isAvailable()) {
+    if (Epoll.isAvailable() && !USE_NIO) {
       return EpollSocketChannel.class;
-    } else if (KQueue.isAvailable()) {
+    } else if (KQueue.isAvailable() && !USE_NIO) {
       return KQueueSocketChannel.class;
     } else {
       return NioSocketChannel.class;
@@ -67,11 +79,11 @@ public final class NettyUtils
    */
   public static EventLoopGroup createDefaultEventLoopGroup(final String name, final int numThreads)
   {
-    if (Epoll.isAvailable()) {
+    if (Epoll.isAvailable() && !USE_NIO) {
       return new EpollEventLoopGroup(
         numThreads,
         new ThreadPerTaskExecutor(new DefaultThreadFactory(name, true, Thread.NORM_PRIORITY)));
-    } else if (KQueue.isAvailable()) {
+    } else if (KQueue.isAvailable() && !USE_NIO) {
       return new KQueueEventLoopGroup(
         numThreads,
         new ThreadPerTaskExecutor(new DefaultThreadFactory(name, true, Thread.NORM_PRIORITY)));
@@ -80,5 +92,17 @@ public final class NettyUtils
         numThreads,
         new ThreadPerTaskExecutor(new DefaultThreadFactory(name, true, Thread.NORM_PRIORITY)));
     }
+  }
+
+
+  /**
+   * Invokes {@link EventLoopGroup#shutdownGracefully(long, long, TimeUnit)} on the supplied worker group. This method
+   * is asynchronous.
+   *
+   * @param  workerGroup  to shutdown
+   */
+  public static void shutdownGracefully(final EventLoopGroup workerGroup)
+  {
+    workerGroup.shutdownGracefully(DEFAULT_SHUTDOWN_QUIET_PERIOD, DEFAULT_SHUTDOWN_MAX_TIMEOUT, TimeUnit.MILLISECONDS);
   }
 }
