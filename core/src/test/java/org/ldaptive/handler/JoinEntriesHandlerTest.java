8bf9e4862e73f51717a37b982a2269720550e534
Miguel Martinez de Espronceda <contact@i2p.es>
2021-02-10 18:16:16 +0100

    Test for JoinEntriesResultHandler.

---
+++ core/src/test/java/org/ldaptive/handler/JoinEntriesHandlerTest.java
@@ -0,0 +1,81 @@
+package org.ldaptive.handler;
+
+import org.ldaptive.LdapAttribute;
+import org.ldaptive.LdapEntry;
+import org.ldaptive.SearchResponse;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.LinkedHashSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Unit test for {@link JoinEntriesResultHandler}.
+ *
+ * @author  Miguel Martinez de Espronceda
+ */
+public class JoinEntriesHandlerTest {
+
+
+    /**
+     * Entry test data.
+     *
+     * @return  test data
+     */
+    @DataProvider(name = "entries")
+    public Object[][] createEntries()
+    {
+        return
+        new Object[][] {
+            new Object[]{
+                new LdapEntry[]{
+                    LdapEntry.builder()
+                            .dn("cn=Homer Simpson,ou=People,dc=ldaptive,dc=org")
+                            .attributes(
+                                    LdapAttribute.builder().name("cn").values("Homer").build(),
+                                    LdapAttribute.builder().name("sn").values("Simpson").build(),
+                                    LdapAttribute.builder().name("uid").values("101").build(),
+                                    LdapAttribute.builder().name("mail").values("hsimpson@tv.com", "pieman@tv.com").build())
+                            .build(),
+                    LdapEntry.builder()
+                            .dn("cn=Bart Simpson,ou=People,dc=ldaptive,dc=org")
+                            .attributes(
+                                    LdapAttribute.builder().name("sn").values("Simpson").build(),
+                                    LdapAttribute.builder().name("cn").values("Bart").build(),
+                                    LdapAttribute.builder().name("uid").values("102").build(),
+                                    LdapAttribute.builder().name("mail").values("bsimpson@tv.com", "bartman@tv.com").build())
+                            .build(),
+                    LdapEntry.builder()
+                            .dn("cn=Lisa Simpson,ou=People,dc=ldaptive,dc=org")
+                            .attributes(
+                                    LdapAttribute.builder().name("cn").values("Lisa").build(),
+                                    LdapAttribute.builder().name("uid").values("103").build(),
+                                    LdapAttribute.builder().name("sn").values("Simpson").build())
+                            .build(),
+                },
+                new LdapEntry[]{
+                    LdapEntry.builder()
+                        .dn("cn=Homer Simpson,ou=People,dc=ldaptive,dc=org")
+                        .attributes(
+                                LdapAttribute.builder().name("cn").values("Homer", "Bart", "Lisa").build(),
+                                LdapAttribute.builder().name("mail").values("hsimpson@tv.com", "pieman@tv.com", "bartman@tv.com", "bsimpson@tv.com").build(),
+                                LdapAttribute.builder().name("sn").values("Simpson").build(),
+                                LdapAttribute.builder().name("uid").values("101", "102", "103").build())
+                        .build()
+                }
+            },
+        };
+    }
+
+    @Test(groups="handlers", dataProvider = "entries")
+    public void apply(final LdapEntry[] actual, final LdapEntry[] expected) {
+        final JoinEntriesResultHandler handler = new JoinEntriesResultHandler();
+        final SearchResponse response = new SearchResponse();
+        response.addEntries(actual);
+        Assert.assertEquals(
+                handler.apply(response).getEntries(),
+                Stream.of(expected).collect(Collectors.toCollection(LinkedHashSet::new)));
+    }
+}
