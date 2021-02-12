a5a0d161273663a8f0f4b8baa110c9e92c3c9472
Daniel Fisher <dfisher@vt.edu>
2021-02-12 18:11:29 -0500

    Fix checkstyle errors.

--- core/src/main/java/org/ldaptive/handler/MergeResultHandler.java
+++ core/src/main/java/org/ldaptive/handler/MergeResultHandler.java
@@ -1,39 +1,52 @@
+/* See LICENSE for licensing and NOTICE for copyright. */
 package org.ldaptive.handler;
 
 import org.ldaptive.LdapUtils;
 import org.ldaptive.SearchResponse;
 
-import java.util.Arrays;
-import java.util.Objects;
 /**
  * Merges the values of the attributes in all entries into a single entry.
  *
- * @author  Middleware Services
+ * @author  Miguel Martinez de Espronceda
  */
-public class MergeResultHandler extends AbstractEntryHandler<SearchResponse> implements SearchResultHandler {
+public class MergeResultHandler extends AbstractEntryHandler<SearchResponse> implements SearchResultHandler
+{
 
-    public MergeResultHandler() {
-    }
+  /** hash code seed. */
+  private static final int HASH_CODE_SEED = 857;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        return true;
-    }
 
-    @Override
-    public int hashCode() {
-        return LdapUtils.computeHashCode(829);
-    }
+  /** Default constructor. */
+  public MergeResultHandler() {}
 
-    public String toString() {
-        return "[" + this.getClass().getName() + "@" + this.hashCode() + "]";
-    }
 
-    @Override
-    public SearchResponse apply(SearchResponse searchResponse) {
-        return SearchResponse.merge(searchResponse);
+  @Override
+  public SearchResponse apply(final SearchResponse searchResponse)
+  {
+    return SearchResponse.merge(searchResponse);
+  }
+
+
+  @Override
+  public boolean equals(final Object o)
+  {
+    if (o == this) {
+      return true;
     }
+    return o instanceof MergeResultHandler;
+  }
+
+
+  @Override
+  public int hashCode()
+  {
+    return LdapUtils.computeHashCode(HASH_CODE_SEED);
+  }
+
+
+  @Override
+  public String toString()
+  {
+    return new StringBuilder("[").append(getClass().getName()).append("@").append(hashCode()).append("]").toString();
+  }
 }
