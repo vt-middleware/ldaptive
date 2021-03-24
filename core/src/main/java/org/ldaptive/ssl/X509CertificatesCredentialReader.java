2e61314adf2b016764787e68cf25d20b1f539c6b
Misagh <misagh.moayyed@gmail.com>
2021-03-24 11:15:27 +0400

    allow credential reader to work with multiple paths

--- core/src/main/java/org/ldaptive/ssl/X509CertificatesCredentialReader.java
+++ core/src/main/java/org/ldaptive/ssl/X509CertificatesCredentialReader.java
@@ -1,24 +1,46 @@
 /* See LICENSE for licensing and NOTICE for copyright. */
 package org.ldaptive.ssl;
 
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
- * Loads X.509 certificate credentials from a classpath, filepath, or stream resource. Supported certificate formats
- * include: PEM, DER, and PKCS7.
+ * Loads X.509 certificate credentials from a classpath, filepath, or stream resource.
+ * When working with filepath, multiple files may be separated using a comma (i.e. {@code cert1.pem,cert2.crt}).
+ * Supported certificate formats include: PEM, DER, and PKCS7.
  *
  * @author  Middleware Services
  */
 public class X509CertificatesCredentialReader extends AbstractCredentialReader<X509Certificate[]>
 {
 
-
+  @Override
+  public X509Certificate[] read(final String path, final String... params)
+      throws IOException, GeneralSecurityException
+  {
+    final String[] paths = path.split(",");
+    return Arrays.stream(paths)
+        .map(individualPath -> {
+          try {
+            return Arrays.asList(super.read(individualPath, params));
+          } catch (final Exception e) {
+            logger.warn(String.format("Unable to read certificate at %s", individualPath), e);
+          }
+          return null;
+        })
+        .filter(Objects::nonNull)
+        .flatMap(List::stream)
+        .toArray(X509Certificate[]::new);
+  }
+  
   @Override
   public X509Certificate[] read(final InputStream is, final String... params)
     throws IOException, GeneralSecurityException
---
+++ core/src/test/java/org/ldaptive/ssl/X509CertificatesCredentialReaderTest.java
@@ -0,0 +1,61 @@
+package org.ldaptive.ssl;
+
+import org.apache.tools.ant.util.FileUtils;
+import org.junit.Test;
+import org.ldaptive.LdapUtils;
+import org.testng.Assert;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.OutputStream;
+import java.security.cert.X509Certificate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Unit test for {@link X509CertificatesCredentialReader}.
+ *
+ * @author  Misagh Moayyed
+ */
+public class X509CertificatesCredentialReaderTest {
+    private static final String MW_CERT =
+        "MIIDhzCCAvCgAwIBAgIJAPpeFAkJP5xgMA0GCSqGSIb3DQEBBQUAMIGKMQswCQYD" +
+        "VQQGEwJVUzERMA8GA1UECBMIVmlyZ2luaWExEzARBgNVBAcTCkJsYWNrc2J1cmcx" +
+        "FjAUBgNVBAoTDVZpcmdpbmlhIFRlY2gxEzARBgNVBAsTCk1pZGRsZXdhcmUxJjAk" +
+        "BgNVBAMTHWxkYXAtdGVzdC0xLm1pZGRsZXdhcmUudnQuZWR1MB4XDTExMDkyNjE2" +
+        "NDczOFoXDTIxMDkyMzE2NDczOFowgYoxCzAJBgNVBAYTAlVTMREwDwYDVQQIEwhW" +
+        "aXJnaW5pYTETMBEGA1UEBxMKQmxhY2tzYnVyZzEWMBQGA1UEChMNVmlyZ2luaWEg" +
+        "VGVjaDETMBEGA1UECxMKTWlkZGxld2FyZTEmMCQGA1UEAxMdbGRhcC10ZXN0LTEu" +
+        "bWlkZGxld2FyZS52dC5lZHUwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAJWf" +
+        "/vBsfFn6sQo57IHrBzMlPARpDI1DJeqH7zl2UeVzeiZDjGiU4ETSjEsvvQRzLfXZ" +
+        "IgJEr1IEAzjCX8wKF4svrmkPK3KN6JvdlknM7Thw5p0NzAh2Bq1R1h7+bUvQJGep" +
+        "aizNM0od/mKrJnOnUCWEgcpG91mWg8b1PphGobeNAgMBAAGjgfIwge8wHQYDVR0O" +
+        "BBYEFMT2Hkcp6JFq242hWfdMOeT3/hZ1MIG/BgNVHSMEgbcwgbSAFMT2Hkcp6JFq" +
+        "242hWfdMOeT3/hZ1oYGQpIGNMIGKMQswCQYDVQQGEwJVUzERMA8GA1UECBMIVmly" +
+        "Z2luaWExEzARBgNVBAcTCkJsYWNrc2J1cmcxFjAUBgNVBAoTDVZpcmdpbmlhIFRl" +
+        "Y2gxEzARBgNVBAsTCk1pZGRsZXdhcmUxJjAkBgNVBAMTHWxkYXAtdGVzdC0xLm1p" +
+        "ZGRsZXdhcmUudnQuZWR1ggkA+l4UCQk/nGAwDAYDVR0TBAUwAwEB/zANBgkqhkiG" +
+        "9w0BAQUFAAOBgQBe0bV5iZyPupNh2zmdH7opuwldz1sxlkRdUQhKSlYsOqgAKDvS" +
+        "DypmR4mqntAULTFGZIdcQ1W8HJcnRc8KuPfNatAV8A9OqMbtDLnmfWkl33JPiDUd" +
+        "fIKCXuG4dZ6nn3RbjlKhXzHYADmJzdQNIC3M9eDQBEYmMy8+mV+ErVebBg==";
+    
+    @Test
+    public void verifyMultipleCertificatesSeparated() throws Exception {
+        final File cert1 = File.createTempFile("cert1", ".pem");
+        try (OutputStream writer = FileUtils.newOutputStream(cert1.toPath(), false)) {
+            writer.write(LdapUtils.base64Decode(MW_CERT));
+            writer.flush();
+        }
+        final File cert2 = File.createTempFile("cert2", ".pem");
+        try (OutputStream writer = FileUtils.newOutputStream(cert2.toPath(), false)) {
+            writer.write(LdapUtils.base64Decode(MW_CERT));
+            writer.flush();
+        }
+        final X509CertificatesCredentialReader reader = new X509CertificatesCredentialReader();
+        final String paths = Stream.of(cert1, cert2).map(file -> "file:" + file.getAbsolutePath()).collect(Collectors.joining(","));
+        final X509Certificate[] certificates = reader.read(paths);
+        Assert.assertEquals(2, certificates.length);
+    }
+
+}
